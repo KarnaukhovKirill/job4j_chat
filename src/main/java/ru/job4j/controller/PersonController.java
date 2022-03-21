2cb032c3ff97db2173128f107f572ba7986ee9f8
KarnaukhovKirill <kirya2361@gmail.com>
2022-03-21 11:45:35 +0300

    Авторизация jwt, через базу данных

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -2,8 +2,11 @@ package ru.job4j.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.model.Person;
+import ru.job4j.model.Role;
+import ru.job4j.repository.RoleRepository;
 import ru.job4j.service.PersonService;
 import ru.job4j.service.Service;
 
@@ -13,9 +16,15 @@ import java.util.List;
 @RequestMapping("/person")
 public class PersonController implements Controller<Person> {
     private final Service<Person> service;
+    private final BCryptPasswordEncoder encoder;
+    private final RoleRepository roleRepository;
 
-    public PersonController(final PersonService personService) {
+    public PersonController(final PersonService personService,
+                            BCryptPasswordEncoder encoder,
+                            RoleRepository roleRepository) {
         this.service = personService;
+        this.encoder = encoder;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -57,5 +66,16 @@ public class PersonController implements Controller<Person> {
         person.setId(id);
         service.delete(person);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<Person> signUp(@RequestBody Person person) {
+        person.setPassword(encoder.encode(person.getPassword()));
+        Role role = roleRepository.findByName("ROLE_USER");
+        person.setRole(role);
+        return new ResponseEntity<>(
+                this.service.save(person),
+                HttpStatus.CREATED
+        );
     }
 }
---
+++ src/main/java/ru/job4j/repository/UserDetailServiceImp.java
@@ -0,0 +1,29 @@
+package ru.job4j.repository;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.job4j.model.Person;
+import org.springframework.security.core.userdetails.User;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailServiceImp implements UserDetailsService {
+    private final PersonRepository users;
+
+    public UserDetailServiceImp(PersonRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Person user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(), user.getPassword(), emptyList());
+    }
+
+}
