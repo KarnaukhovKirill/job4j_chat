0b0873e079f8af1590f384c3f123fcb38fbefa2f
KarnaukhovKirill <kirya2361@gmail.com>
2022-04-14 22:01:55 +0300

    add liquibase

--- src/main/java/ru/job4j/Job4jChatApplication.java
+++ src/main/java/ru/job4j/Job4jChatApplication.java
@@ -1,9 +1,11 @@
 package ru.job4j;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Job4jChatApplication {
@@ -15,5 +17,13 @@ public class Job4jChatApplication {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 }
